29a7122df0808d4414cac6a9a9680897759140ee
Abdulmumin Abdulkarim <abdulmumineabdulkarim@gmail.com>
2017-12-19 00:00:55 +0100

    greeting

--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.createConnection("mongodb://127.0.0.1:27017/admin", { useMongoClient: true });
+mongoose.createConnection("mongodb://john:doe@localhost:27017/databaseName", { useMongoClient: true });
